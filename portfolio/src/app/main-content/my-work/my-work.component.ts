50328d6f4ad4c9bc725725a65b50663dde31abe6
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-26 11:59:35 +0200

    ngfor for projects with josn and styling

--- portfolio/src/app/main-content/my-work/my-work.component.ts
+++ portfolio/src/app/main-content/my-work/my-work.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-work',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-work.component.html',
-  styleUrl: './my-work.component.scss'
+  styleUrl: './my-work.component.scss',
 })
 export class MyWorkComponent {
+  projects = [
+    {
+      mockup: 'join.png',
+      name: 'Join',
+      projectLanguage: 'JavaScript | HTML | CSS',
+      content:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      link: 'join'
+    },
+    {
+      mockup: 'pollo_loco.png',
+      name: 'Pollo Loco',
+      projectLanguage: 'JavaScript | HTML | CSS',
+      content:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+        link: 'El-Polo-Loco'
+      },
+    {
+      mockup: 'simple_crm.png',
+      name: 'Simple CRM',
+      projectLanguage: 'Angular | Firebase',
+      content:
+        'A very Simple Customer Relationship Management system working with CRUD functionality.',
+        link: 'simple-crm'
+      },
+  ];
+
 
 }
