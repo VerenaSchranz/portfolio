5d663c7057d0ed1bd94c418684800413e3a171a2
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-27 07:04:05 +0200

    styling responsive adding circles

--- portfolio/src/app/main-content/my-work/my-work.component.ts
+++ portfolio/src/app/main-content/my-work/my-work.component.ts
@@ -9,6 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './my-work.component.scss',
 })
 export class MyWorkComponent {
+  
+  isEven(index: number): boolean {
+    return index % 2 === 0;
+  }
+  
   projects = [
     {
       mockup: 'join.png',
@@ -16,7 +21,8 @@ export class MyWorkComponent {
       projectLanguage: 'JavaScript | HTML | CSS',
       content:
         'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      link: 'join'
+      link: 'join',
+      liveTest: '#',
     },
     {
       mockup: 'pollo_loco.png',
@@ -24,7 +30,8 @@ export class MyWorkComponent {
       projectLanguage: 'JavaScript | HTML | CSS',
       content:
         'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
-        link: 'El-Polo-Loco'
+        link: 'El-Polo-Loco',
+        liveTest: '#',
       },
     {
       mockup: 'simple_crm.png',
@@ -32,7 +39,8 @@ export class MyWorkComponent {
       projectLanguage: 'Angular | Firebase',
       content:
         'A very Simple Customer Relationship Management system working with CRUD functionality.',
-        link: 'simple-crm'
+        link: 'simple-crm',
+        liveTest: '#',
       },
   ];
 
