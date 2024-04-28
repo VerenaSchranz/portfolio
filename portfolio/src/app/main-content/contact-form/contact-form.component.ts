0df21b619b006278486e9e6c28e7c724a1cca5e4
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-28 08:32:42 +0200

    contactform

--- portfolio/src/app/main-content/contact-form/contact-form.component.ts
+++ portfolio/src/app/main-content/contact-form/contact-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
-
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+  onSubmit(ngForm: NgForm) {
+    if(ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData)
+    }
+  }
 }
