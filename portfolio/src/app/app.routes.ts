aa4a3001108bf1d07cde6afdb990283fb1d7f953
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-05-11 06:59:49 +0200

    routing try

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import { Routes } from '@angular/router';
-import { ImprintComponent } from './imprint/imprint.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
-
-
+import { ImpressumComponent } from './impressum/impressum.component';
 
 export const routes: Routes = [
-    { path: '', component: MainContentComponent },
-    { path: 'imprint', component: ImprintComponent },
-  ];
+  { path: '', component: MainContentComponent },
+  { path: 'impressum', component: ImpressumComponent },
+];
+
+
+export class AppRoutingModule { }
